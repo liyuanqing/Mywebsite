7f83d2abcb0f39a037718556aaeaef1489b9f74c
Lyq <1069795183@qq.com>
2017-10-09 16:38:06 +0800

    添加自我介绍

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Mywebsite
 个人网站
+* 采用html模板，易上手开发，符合前端开发习惯，模板采用ejs,具体语法请查看[ejs语法](https://github.com/mde/ejs) .
--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title:'Express' });
+});
+/* 简历 */
+router.get('/SelfIntroduction', function(req, res, next) {
+  res.render('SelfIntroduction');
 });
 
 module.exports = router;
