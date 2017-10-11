8ca76a44e9bd579463723cbec0c9189678cc9cc6
Lyq <1069795183@qq.com>
2017-10-11 14:38:53 +0800

    index.html change

--- myapp/app.js
+++ myapp/app.js
@@ -47,7 +47,8 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error',{
+    layout: 'layout'});
 });
 
 //数据库连接
